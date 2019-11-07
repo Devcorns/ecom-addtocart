a0cc81af4614bd7ae640f194aa0510801fc20f14
Prakhar Mathur <prakhar@isglobalweb.com>
2019-11-07 04:52:56 -0800

    add to cart func

--- src/app/products/products-listing/products-listing.component.ts
+++ src/app/products/products-listing/products-listing.component.ts
@@ -4,34 +4,39 @@ import { AddtocartService } from 'src/app/services/addtocart.service';
 @Component({
   selector: 'app-products-listing',
   templateUrl: './products-listing.component.html',
-  styleUrls: ['./products-listing.component.sass']
+  styleUrls: ['./products-listing.component.scss']
 })
 export class ProductsListingComponent implements OnInit {
-
+  productItem = 0;
+  qty = 1;
   products = [ 
     { 
       name:"Product 1", 
       price:"17",
       uid:1,
-      rating:5
+      rating:5,
+      qty:1
     },
     { 
       name:"Product 2", 
       price:"15",
       uid:2,
-      rating:3
+      rating:3,
+      qty:1
     },
     { 
       name:"Product 3", 
       price:"11",
       uid:3,
-      rating:1
+      rating:1,
+      qty:1
     },
     { 
       name:"Product 4", 
       price:"18",
       uid:4,
-      rating:2
+      rating:2,
+      qty:1
     }
 ];
 
@@ -41,7 +46,9 @@ export class ProductsListingComponent implements OnInit {
   }
 
   addToCart( event, data ) {
+    console.log(data.uid);
     this.addToCartService.addToCart(data);
+    this.productItem = this.addToCartService.cartItems.length;
   }
 
   showProducts() {
@@ -49,6 +56,23 @@ export class ProductsListingComponent implements OnInit {
       alert(this.addToCartService.cartItems);
     } else {
       alert("Sorry You have no items")
+    }
+  }
+
+  resetCart() {
+    this.addToCartService.cartItems = [];
+    this.productItem = 0;
+  }
+
+  increaseQty(event) { 
+     event.target.closest(".product-qty").querySelectorAll(".qntty")[0].innerText = +event.target.closest(".product-qty").querySelectorAll(".qntty")[0].innerText + 1;
+  }
+  
+  decreaseQty(event) {
+    if(+event.target.closest(".product-qty").querySelectorAll(".qntty")[0].innerText > 1){
+      event.target.closest(".product-qty").querySelectorAll(".qntty")[0].innerText = +event.target.closest(".product-qty").querySelectorAll(".qntty")[0].innerText -1;
+    } else {
+      alert("Do you wish to delete the selected product");
     }
   }
 
